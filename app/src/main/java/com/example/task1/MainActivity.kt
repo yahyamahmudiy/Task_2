159f869cfafed4693ec1605b5de55b661d356695
Yahya Mahmudiy <mustofamahmudiy@gmail.com>
2021-12-22 20:02:04 +0500

    I set button to open second Activity

--- app/src/main/java/com/example/task1/MainActivity.kt
+++ app/src/main/java/com/example/task1/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.example.task1
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "First Activity"
+
+        val changeActivityBTN = findViewById<Button>(R.id.button)
+        changeActivityBTN.setOnClickListener { changeActivity() }
+
+    }
+    fun changeActivity() {
+        val intent = Intent(this, MainActivity2::class.java)
+        startActivity(intent)
     }
 }
---
+++ app/src/main/java/com/example/task1/MainActivity2.kt
@@ -0,0 +1,14 @@
+package com.example.task1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+
+        val actionBar = supportActionBar
+        actionBar!!.title = "Second Activity"
+    }
+}
